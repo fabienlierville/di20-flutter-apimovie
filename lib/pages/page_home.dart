b237eb00632901bf17b21c1cf524d6ba617b1094
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-05 12:29:42 +0100

    Api Movie

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -1,3 +1,5 @@
+import 'package:api_movies/models/api_movie.dart';
+import 'package:api_movies/models/movie.dart';
 import 'package:flutter/material.dart';
 
 class PageHome extends StatefulWidget {
@@ -8,13 +10,35 @@ class PageHome extends StatefulWidget {
 }
 
 class _PageHomeState extends State<PageHome> {
+  List<Movie> movies = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Films"),
+        actions: [
+          IconButton(
+            onPressed: getPopularMovie,
+            icon: Icon(Icons.refresh),
+          )
+        ],
       ),
       body: Center(),
     );
+  }
+
+  Future<void> getPopularMovie() async{
+    ApiMovie api = ApiMovie();
+    Map<String,dynamic> map =  await api.getPopular();
+    if(map["code"] == 200){
+      movies = Movie.moviesFromApi(map["body"]);
+      movies.shuffle();
+      movies.forEach((Movie movie) {
+        print(movie.title);
+      });
+    }else{
+      //Todo
+    }
   }
 }
