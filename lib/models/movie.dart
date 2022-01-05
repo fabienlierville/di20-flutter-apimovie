02cb20ab217421a060dde7e989fa6f4fbd5275c5
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-05 12:03:32 +0100

    Model Movie

---
+++ lib/models/movie.dart
@@ -0,0 +1,28 @@
+class Movie{
+  final int id;
+  final String title;
+  final String overview;
+  final DateTime releaseDate;
+  final String imageUrl;
+  final double vote;
+
+  Movie({required this.id,required  this.title,required  this.overview,required  this.releaseDate,required  this.imageUrl, required this.vote});
+
+
+  static List<Movie> moviesFromApi(Map<String,dynamic> jsonBody){
+    List<Movie> l = [];
+    List<dynamic> results = jsonBody["results"];
+    results.forEach((data) {
+      Movie movie = Movie(
+        id: data["id"],
+        title: data["title"],
+        overview: data["overview"],
+        imageUrl: data["poster_path"],
+        releaseDate: DateTime.parse(data["release_date"]),
+        vote: double.tryParse( data["vote"].toString()) ?? 0
+      );
+    });
+    return l;
+  }
+
+}
